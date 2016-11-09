8bfc35f4a6428f58b2be522156dd561dea259830
biruzka <biruzka@gmail.com>
2016-11-09 15:43:44 +0300

    documents and prototypes

--- layouts/actions/actions.js
+++ layouts/actions/actions.js
@@ -21,3 +21,7 @@ $(document).ready(function() {
 $(document).ready(function(){
     $('.collapsible').collapsible();
 });
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
